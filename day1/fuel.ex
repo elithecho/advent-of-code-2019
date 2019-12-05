6855f76e2f8f4e477a67e9de35468a6ec7c6fed4
Chong Hui <iamchoonggg@gmail.com>
2019-12-05 10:24:49 +0800

    day 1 part 2

--- day1/fuel.ex
+++ day1/fuel.ex
@@ -1,10 +1,16 @@
 defmodule Fuel do
   def from_mass(mass) do
-    mass / 3
-    |> Float.floor
-    |> (&(&1 - 2)).()
-    |> trunc
+    floor(mass / 3) - 2
   end
+
+  def total(total, mass) when mass >= 0 do
+    fuel = from_mass(mass)
+    sum = total + max(0, fuel)
+
+    total(sum, fuel)
+  end
+
+  def total(total, _mass), do: total
 
   def read_from_file(filename) do
     {:ok, file} = File.read(filename)
@@ -13,7 +19,7 @@ defmodule Fuel do
     |> String.trim
     |> String.split("\n")
     |> Enum.map(&String.to_integer/1)
-    |> Enum.map(&from_mass/1)
+    |> Enum.map(fn x -> total(0, x) end)
     |> Enum.sum
   end
 end
